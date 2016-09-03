2d63ab4f6225ef4fddf1cecfc0822b650b8a12e8
r3dskhie <wilf.pineda@yahoo.com>
2016-09-03 11:09:39 +0800

    Update Route1.lua

--- Quest/Kanto/Route1.lua
+++ Quest/Kanto/Route1.lua
@@ -18,60 +18,60 @@ local dialogs = {
 	
 }
 
-local PalletTownQuest = Quest:new()
-function PalletTownQuest:new()
-	return Quest.new(PalletTownQuest, name, description, _, dialogs)
+local Route1Quest = Quest:new()
+function Route1Quest:new()
+	return Quest.new(Route1Quest, name, description, _, dialogs)
 end
 
-function PalletTownQuest:isDoable()
+function Route1Quest:isDoable()
 	if self:hasMap() then
 		return true
 	end
 	return false
 end
 
-function PalletTownQuest:isDone()
+function Route1Quest:isDone()
 	return getMapName() == "Route 1"
 end
 
 
-function PalletTownQuest:PalletTown()
+function Route1Quest:Route1()
 	if isNpcOnCell(6,7) then
-				pushDialogAnswer(butter)
-			log("---Headbutting 1st tree---")
-				return talkToNpcOnCell(6,7)	--Tree 1
-			elseif isNpcOnCell(19,5) then
-				pushDialogAnswer(butter)
-			log("---Headbutting 2nd tree---")
-				return talkToNpcOnCell(19,5)	--Tree 2
-			elseif isNpcOnCell(13,23) then
-				pushDialogAnswer(butter)
-			log("---Headbutting 3rd tree---")
-				return talkToNpcOnCell(13,23)	--Tree 3
-			elseif isNpcOnCell(17,23) then
-				pushDialogAnswer(butter)
+		pushDialogAnswer(butter)
+	log("---Headbutting 1st tree---")
+		return talkToNpcOnCell(6,7)	--Tree 1
+	elseif isNpcOnCell(19,5) then
+		pushDialogAnswer(butter)
+	log("---Headbutting 2nd tree---")
+		return talkToNpcOnCell(19,5)	--Tree 2
+	elseif isNpcOnCell(13,23) then
+		pushDialogAnswer(butter)
+	log("---Headbutting 3rd tree---")
+		return talkToNpcOnCell(13,23)	--Tree 3
+	elseif isNpcOnCell(17,23) then
+		pushDialogAnswer(butter)
 			log("---Headbutting 4th tree---")
-				return talkToNpcOnCell(17,23)	--Tree 4
-			elseif isNpcOnCell(14,28) then
-				pushDialogAnswer(butter)
-			log("---Headbutting 5th tree---")
-				return talkToNpcOnCell(14,28)	--Tree 5
-			elseif isNpcOnCell(17,35) then
-				pushDialogAnswer(butter)
-			log("---Headbutting 6th tree---")
-				return talkToNpcOnCell(17,35)	--Tree 6
-			elseif isNpcOnCell(9,37) then
-				pushDialogAnswer(butter)
-			log("---Headbutting 7th tree---")
-				return talkToNpcOnCell(9,37)	--Tree 7
-			elseif isNpcOnCell(28,43) then
-				pushDialogAnswer(butter)
-			log("---Headbutting Last tree---")
-				return talkToNpcOnCell(28,43)	--Tree 8
-			else
-			log("---"..getMapName().." Cleared... Moving to next Map---")
-				moveToMap("Route 1 Stop House")
-			end
+		return talkToNpcOnCell(17,23)	--Tree 4
+	elseif isNpcOnCell(14,28) then
+		pushDialogAnswer(butter)
+	log("---Headbutting 5th tree---")
+		return talkToNpcOnCell(14,28)	--Tree 5
+	elseif isNpcOnCell(17,35) then
+		pushDialogAnswer(butter)
+	log("---Headbutting 6th tree---")
+		return talkToNpcOnCell(17,35)	--Tree 6
+	elseif isNpcOnCell(9,37) then
+		pushDialogAnswer(butter)
+	log("---Headbutting 7th tree---")
+		return talkToNpcOnCell(9,37)	--Tree 7
+	elseif isNpcOnCell(28,43) then
+		pushDialogAnswer(butter)
+	log("---Headbutting Last tree---")
+		return talkToNpcOnCell(28,43)	--Tree 8
+	else
+	log("---"..getMapName().." Cleared... Moving to next Map---")
+		moveToMap("Route 1 Stop House")
+	end
 end
 
 function PalletTownQuest:battle()
